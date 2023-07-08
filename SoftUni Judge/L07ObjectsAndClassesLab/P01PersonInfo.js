5033fc6edc568d8e0fc48ea3b75185fc7bd6dc71
Mihail Slavov <mihail_slavov@icloud.com>
2023-07-08 22:39:15 +0300

    JS

---
+++ SoftUni Judge/L07ObjectsAndClassesLab/P01PersonInfo.js
@@ -0,0 +1,12 @@
+function info(firstName, lastName,age ) {
+
+    let person= {};
+
+    person.firstName = firstName,
+    person.lastName =  lastName,
+    person.age =  age
+return person;
+}
+
+info("Peter", "Pan", "20");
+info("George", "Smith", "18");
